d1682e16b7e34f16c9578223589d17bbb85dd1c2
abr-ya <gamer05@list.ru>
2022-05-26 08:09:57 +0300

    init: Start Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SimpleForm from "components/SimpleForm";
+
+const App = (): JSX.Element => (
+  <main>
+    <h1>Hello, React Form!</h1>
+    <section>
+      <SimpleForm />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/components/SimpleForm.tsx
@@ -0,0 +1,39 @@
+import { useState, FormEvent } from "react";
+import logIn from "./log-in.png";
+
+const SimpleForm = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("submitForm", login, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <header>
+        <img width="60" src={logIn} alt="log in" />
+      </header>
+      <label htmlFor="login">Login</label>
+      <input
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+        id="login"
+        name="login"
+        type="text"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        id="password"
+        name="password"
+        type="password"
+      />
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
+
+export default SimpleForm;
